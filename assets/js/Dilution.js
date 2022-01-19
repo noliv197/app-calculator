8a0fdfa48af18b42fd55f4a7efd69a852371b21f
Daniel Gusmão de Morais <50711061+Danielmoraisg@users.noreply.github.com>
2022-01-19 09:11:01 -0300

    correct issue with double variables

--- assets/js/Dilution.js
+++ assets/js/Dilution.js
@@ -5,42 +5,43 @@ class Dilution
 	{
 		// user variables
 		// one has to be empty
-		var c1 = "1";
-		var v1 = "1";
-		var c2 = "1";
-		var v2 = "1";
-		if (isNaN(c1))
+		var c1;
+		var v1 = "1.0";
+		var c2 = "1.0";
+		var v2 = "1.0";
+		if (!c1)
 		{
-			var V1 = Double.parseDouble(v1);
-			var C2 = Double.parseDouble(c2);
-			var V2 = Double.parseDouble(v2);
+    	
+			var V1 = parseFloat(v1);
+			var C2 = parseFloat(c2);
+			var V2 = parseFloat(v2);
 			var C1 = (C2 * V2) / V1;
 			console.log("Use " + new String(C1).toString());
 		}
-		else if (isNaN(c2))
+		else if (!c2)
 		{
-			var C1 = Double.parseDouble(c1);
-			var V1 = Double.parseDouble(v1);
-			var V2 = Double.parseDouble(v2);
+			var C1 = parseFloat(c1);
+			var V1 = parseFloat(v1);
+			var V2 = parseFloat(v2);
 			var C2 = (C1 * V1) / V2;
 			console.log("Use " + new String(C2).toString());
 		}
-		else if (isNaN(v1))
+		else if (!v1)
 		{
-			var C1 = Double.parseDouble(c1);
-			var C2 = Double.parseDouble(c2);
-			var V2 = Double.parseDouble(v2);
+			var C1 = parseFloat(c1);
+			var C2 = parseFloat(c2);
+			var V2 = parseFloat(v2);
 			var V1 = (C2 * V2) / C1;
 			console.log("Use " + new String(V1).toString());
 		}
-		else if (isNaN(v2))
+		else if(!v2)
 		{
-			var C1 = Double.parseDouble(c1);
-			var V1 = Double.parseDouble(v1);
-			var C2 = Double.parseDouble(c2);
+			var C1 = parseFloat(c1);
+			var V1 = parseFloat(v1);
+			var C2 = parseFloat(c2);
 			var V2 = (C1 * V1) / C2;
 			console.log("Use " + new String(V2).toString());
 		}
 	}
 }
-Dilution.main([]);
+Dilution.main()
