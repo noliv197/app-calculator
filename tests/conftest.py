363fdce5ff3d460acff59b2a88b05d311ed01c79
Daniel Gusmão de Morais <danielgmoraisg@gmail.com>
2023-02-23 09:33:31 -0300

    ✅ Finish user tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,11 +11,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.models import Base
 from app.database import get_db
 from app.routers import user
+from app.utils import hash_password
 
 from pytest_postgresql import factories
 from pytest_postgresql.janitor import DatabaseJanitor
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm.session import Session
+from sqlalchemy.sql import text
+from app.oauth2 import AuthJWT
+
 
 test_db = factories.postgresql_proc(port=None, dbname="test_db")
 
@@ -24,7 +28,7 @@ def start_application():
     app.include_router(user.router)
     return app
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def client(app: FastAPI, db_session: Session):
     """
     Create a new FastAPI TestClient that uses the `db_session` fixture to override
@@ -39,7 +43,7 @@ def client(app: FastAPI, db_session: Session):
     with TestClient(app) as client:
         yield client
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def db_session(test_db):
     """Session for SQLAlchemy."""
     pg_host = test_db.host
@@ -57,7 +61,7 @@ def db_session(test_db):
             yield session
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def app(db_session):
     """
     Create a fresh database on each test case.
@@ -67,6 +71,19 @@ def app(db_session):
     yield _app
     #Base.metadata.drop_all(db_session.bind)
 
+@pytest.fixture(scope="function")
+def user_created(db_session):
+    db_session.execute(text(f'''INSERT INTO public.users
+                        ("name", email, "password", created_at, updated_at)
+                        VALUES('John', 'john@mail.com', '{hash_password('secret_password')}', now(), now());'''))
 
-
+@pytest.fixture(scope="function")
+def register_payload():
+    yield {
+            "name": "John",
+            "email": "john@mail.com",
+            "password": "secret_password",
+            "passwordConfirm": "secret_password"
+            }
+    
 
--- tests/test_routes/test_users.py
+++ tests/test_routes/test_users.py
@@ -10,15 +10,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.main import app
 from app.models import User
 
-
-register_payload = {
-                    "name": "John",
-                    "email": "John@example.com",
-                    "password": "secret_password",
-                    "passwordConfirm": "secret_password"
-                    }
-
-def test_register(client, db_session):
+def test_register(client, db_session, register_payload):
     wrong_password_payload = register_payload.copy()
     wrong_password_payload["passwordConfirm"] = "wrong_password"
     response = client.post("/register", json=wrong_password_payload)
@@ -28,21 +20,24 @@ def test_register(client, db_session):
     response = client.post("/register", json=register_payload)
     assert response.content == b'{"detail":"Account already exist"}'
     
-
-def test_login(client):
+def test_login(client, register_payload, user_created, db_session):
     login_payload = {
                     "email": register_payload['email'],
                     "password": register_payload['password']
                     }
     response = client.post("/login", json=login_payload)
-    print(response.content)
     assert response.status_code == 200
-    assert response.json()["access_token"] is not None
+    access_token = response.json()["access_token"]
+    assert access_token is not None
+    response = client.get("/refresh")
+    refresh_token = response.json()["access_token"]
+    assert refresh_token is not None
+    response = client.get("/me")
+    assert response.status_code == 200
 
-def test_delete(client ,db_session):
+def test_delete(client ,db_session, user_created, register_payload):
     stmt = select(User).where(User.name == "John")
     user_id = db_session.execute(stmt).first()[0].id
     response = client.delete(f"/{user_id}")
-    print(response.content)
     assert response.status_code == 200
     assert not db_session.execute(text(f"select * from users where email = '{register_payload['email']}'")).fetchall()
